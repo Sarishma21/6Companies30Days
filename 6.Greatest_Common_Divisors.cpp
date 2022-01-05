6d896fff4a655dd15e58390d319b9663e357b915
Sarishma21 <71546448+Sarishma21@users.noreply.github.com>
2022-01-05 22:33:45 +0530

    Delete 6.Greatest_Common_Divisors.cpp

--- 6.Greatest_Common_Divisors.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-	string gcdOfStrings(string& str1, string& str2) {
-		int result = gcd(str1.length(), str2.length());
-		return (str1 + str2 == str2 + str1) ? str1.substr(0, result) : "";
-	}
-};
